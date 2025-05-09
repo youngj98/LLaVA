870122d5e80c88c52b9c803acde8113effaa9694
youngj98 <yjeon98@hanyang.ac.kr>
2025-05-09 23:25:06 +0900

    init

---
+++ sample.py
@@ -0,0 +1,33 @@
+from llava.model.builder import load_pretrained_model
+from llava.mm_utils import get_model_name_from_path
+from llava.eval.run_llava import eval_model
+
+model_path = "liuhaotian/llava-v1.5-7b"
+
+tokenizer, model, image_processor, context_len = load_pretrained_model(
+    model_path=model_path,
+    model_base=None,
+    model_name=get_model_name_from_path(model_path)
+)
+
+model_path = "liuhaotian/llava-v1.5-7b"
+# prompt = "What are the things I should be cautious about when I visit here?"
+prompt = "여기 방문 시 주의해야 할 점은 무엇입니까?"
+# image_file = "https://llava-vl.github.io/static/images/view.jpg"
+image_file = "/home/ailab/Desktop/young/LLaVA/20240710_004749.jpg"
+
+args = type('Args', (), {
+    "model_path": model_path,
+    "model_base": None,
+    "model_name": get_model_name_from_path(model_path),
+    "query": prompt,
+    "conv_mode": None,
+    "image_file": image_file,
+    "sep": ",",
+    "temperature": 0,
+    "top_p": None,
+    "num_beams": 1,
+    "max_new_tokens": 512
+})()
+
+eval_model(args)
