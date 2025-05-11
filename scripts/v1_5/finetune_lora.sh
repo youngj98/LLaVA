1a6316859319b3850b188076eb44aa23fab50b23
youngj98 <yjeon98@hanyang.ac.kr>
2025-05-12 01:32:52 +0900

    for debug

--- scripts/v1_5/finetune_lora.sh
+++ scripts/v1_5/finetune_lora.sh
@@ -18,9 +18,9 @@ deepspeed llava/train/train_mem.py \
     --bf16 True \
     --output_dir ./checkpoints/llava-v1.5-13b-lora \
     --num_train_epochs 1 \
-    --per_device_train_batch_size 16 \
+    --per_device_train_batch_size 4 \
     --per_device_eval_batch_size 4 \
-    --gradient_accumulation_steps 1 \
+    --gradient_accumulation_steps 4 \
     --evaluation_strategy "no" \
     --save_strategy "steps" \
     --save_steps 50000 \
